15d3d3b0502f64fd4b82ed2e8224452f257f3945
geriosla99 <geriosla@gmail.com>
2022-08-08 17:46:56 -0500

    Agregar archivos de proyecto.

---
+++ Matrices escalonadas/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Matrices_escalonadas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //tipo[][] nombre = new tipo [filas][];
+
+            //int[] numeros = new int[4] { 23, 13, 5, 6 };
+            int i;
+            int j;
+            //Mostramos cuántos  elementos tiene la matriz unidimensional
+            //Console.WriteLine(numeros.length);
+
+            double[][] ventas = new double[4][] { new double[] { 100, 120, 205 }, 
+                                                    new double[]  { 115, 196, 300 }, 
+                                                    new double[]  { 157, 172, 245 },
+                                                    new double[]  { 130, 180, 281 } };
+            //Console.WriteLine(ventas2D.Length);
+
+            for(i=0; i < ventas.Length; i++)
+            {
+                Console.WriteLine("Elemento {0}", i);
+                for(j=0; j<ventas[i].Length; j++)
+                {
+                    Console.WriteLine(ventas[i][j]);
+                }
+            }
+
+        }
+    }
+}
